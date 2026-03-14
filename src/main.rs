651bcd7ee08c494d14927ced194e45db2431952e
Revanth Madupoju <revanthmadupoju@gmail.com>
2026-03-14 10:08:59 +0500

    refactor: Config::new() return type to Result<Config, &str>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
@@ -7,21 +8,24 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
-            panic!("Not enough arguments");
+            return Err("Not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for '{}'", config.query);
     println!("In file '{}'", config.filename);
