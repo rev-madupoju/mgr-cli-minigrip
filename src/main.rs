a2f6e4a3642ad4d959621d190b782b091a6b7aec
Revanth Madupoju <revanthmadupoju@gmail.com>
2026-03-14 10:20:19 +0500

    refactor: moved main() file read logic to run() with error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -19,6 +20,14 @@ impl Config {
     }
 }
 
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -30,8 +39,8 @@ fn main() {
     println!("Searching for '{}'", config.query);
     println!("In file '{}'", config.filename);
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong while reading the file");
-
-    println!("With text:\n{}", contents);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
